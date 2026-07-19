0db457b48fcf08371c58f907bdcf0e493380e674
layn-build <build@layn.su>
2026-07-19 23:02:26 +0000

    fix: show debugPrint only to avoid Category conflict

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:http/http.dart' as http;
 import '../constants.dart';
 import '../models/models.dart';
